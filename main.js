1ec0bdfc365fc005a8c9b90b6ab5ba8fd0877846
praathul <44019979+athulpram@users.noreply.github.com>
2019-01-06 19:51:46 +0530

    Created function to verify the cell clicked
    Still need to take care of edge cases

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let mines = [];
+let gridSize = 0;
 const generateGrid = function(gridSize) {
   let table = document.createElement("table");
   for (let row = 0; row < gridSize; row++) {
@@ -13,7 +15,7 @@ const generateGrid = function(gridSize) {
 const generateRow = function(stratCell, gridSize) {
   let row = document.createElement("tr");
   for (let cell = stratCell; cell < gridSize + stratCell; cell++) {
-    row.appendChild(generateCell(cell));
+    row.appendChild(generateCell(cell, gridSize));
   }
   return row;
 };
@@ -23,7 +25,7 @@ const generateCell = function(id) {
   cell.setAttribute("id", id);
   cell.setAttribute(
     "style",
-    "height : 100px; width : 100px; border : 1px solid white; background : black"
+    "height : 100px; width : 100px; border : 1px solid white; background : black; color : white"
   );
   cell.setAttribute("onmousedown", "performButtonAction(event,this.id)");
   return cell;
@@ -36,10 +38,47 @@ const performButtonAction = function(event, id) {
   verfiyCell(id);
 };
 
+const isMine = function(id) {
+  return mines.includes(id);
+};
+
 const verfiyCell = function(id) {
-  alert("left button clicked ");
+  console.log("is mine", isMine(id));
+  if (isMine(+id)) {
+    alert("you lost game....");
+    return 0;
+  }
+  console.log("here assign " + getNeighbourMines(id) + "id is" + id);
+  document.getElementById(id).innerText = getNeighbourMines(+id);
+};
+
+const getNeighbourMines = function(id) {
+  let neighbours = [
+    id + 1,
+    id - 1,
+    id + gridSize,
+    id - gridSize,
+    id + gridSize + 1,
+    id - gridSize - 1,
+    id + gridSize - 1,
+    id - gridSize + 1
+  ];
+  return neighbours.reduce((count, cell) => count + mines.includes(+cell), 0);
 };
 
 const flagCell = function(id) {
   alert("right button clicked");
+};
+
+const startGame = function(groundSize, numberOfMines) {
+  // ground = new Array(groundSize).fill(0);
+  gridSize = groundSize;
+  for (let mine = 0; mine < numberOfMines; mine++) {
+    let mineLocation;
+    do {
+      mineLocation = Math.floor(Math.random() * (groundSize * groundSize));
+    } while (mines.includes(mineLocation));
+    mines.push(mineLocation);
+  }
+  generateGrid(5);
 };
